1904e675339c5bcba1fa235cd9568ac413cc3be9
jen-soft <jen.soft.master@gmail.com>
2016-07-05 11:49:39 +0300

    new django project, add apps: spider, spider_admin

---
+++ grab_doc/wsgi.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+WSGI config for grab_doc project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grab_doc.settings")
+
+application = get_wsgi_application()
